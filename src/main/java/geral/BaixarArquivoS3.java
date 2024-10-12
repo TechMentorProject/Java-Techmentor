170300d80483a562a811d70b14ed3060170f0479
Matheusbarros54 <matheus.barros@sptech.school>
2024-10-12 13:47:07 -0300

    Métodos para download de arquivos do s3

---
+++ src/main/java/geral/BaixarArquivoS3.java
@@ -0,0 +1,55 @@
+package geral;
+
+import software.amazon.awssdk.core.sync.ResponseTransformer;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class BaixarArquivoS3 {
+    public static void main(String[] args) throws IOException {
+
+        S3Client s3Client = new S3Provider().getS3Client();
+        String caminhoArquivo = "./base de dados";
+        String nomeObjeto;
+        Path caminho = Paths.get(caminhoArquivo);
+        Path caminhoObjeto;
+
+        List<Bucket> buckets =  s3Client.listBuckets().buckets();
+
+        for (Bucket bucket : buckets) {
+            System.out.println("Bucket: " + bucket.name());
+        }
+        ListObjectsRequest listObjects = ListObjectsRequest.builder().bucket("s3-bucket-atividade").build();
+
+        List<S3Object> objects = s3Client.listObjects(listObjects).contents();
+        for (S3Object object : objects) {
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket("s3-bucket-atividade")
+                    .key(object.key())
+                    .build();
+
+            nomeObjeto = object.key();
+            caminhoObjeto = Paths.get(caminhoArquivo + nomeObjeto);
+            System.out.println(object.key());
+
+            InputStream objectContent = s3Client.getObject(getObjectRequest, ResponseTransformer.toInputStream());
+
+            System.out.println(caminhoObjeto);
+            if (Files.exists(caminhoObjeto)) {
+                // Se o arquivo existir, apaga o arquivo
+                System.out.println(caminhoObjeto);
+                Files.delete(caminhoObjeto);
+                System.out.println("Arquivo existente removido: " + caminhoObjeto);
+            }
+            Files.copy(objectContent, new File(caminhoArquivo + object.key()).toPath());
+        }
+    }
+}
+
---
+++ src/main/java/geral/S3Provider.java
@@ -0,0 +1,36 @@
+package geral;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+import software.amazon.awssdk.core.sync.ResponseTransformer;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
+import software.amazon.awssdk.services.s3.model.S3Object;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.List;
+
+public class S3Provider {
+
+    private final AwsSessionCredentials credentials;
+
+    public S3Provider() {
+        this.credentials = AwsSessionCredentials.create(
+                System.getenv("AWS_ACCESS_KEY_ID"),
+                System.getenv("AWS_SECRET_ACCESS_KEY"),
+                System.getenv("AWS_SESSION_TOKEN")
+        );
+    }
+
+    public S3Client getS3Client() {
+        return S3Client.builder()
+                .region(Region.US_EAST_1)
+                .credentialsProvider(() -> credentials)
+                .build();
+    }
+
+}
